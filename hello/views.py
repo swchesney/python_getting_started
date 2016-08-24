fcd711b263e71592f7a74798169d01d2d62fa4cb
Scott Chesney <swchesney@gmail.com>
2016-08-24 16:14:39 -0700

    times

--- hello/views.py
+++ hello/views.py
@@ -1,18 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import os
+
 from .models import Greeting
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, 'index.html')
+    
 def tea(request):
     r = requests.get('http://httpbin.org/status/418')
     print r.text
     return HttpResponse('<pre>' + r.text + '</pre>')
 
-
+def ostimes(request):
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello! ' * times)
+    
 def db(request):
 
     greeting = Greeting()
